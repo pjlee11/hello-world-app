91e1f482914d0e5668eae0b258bdf7627da903ea
Phil Lee <phil.lee.ext@bbc.co.uk>
2019-01-24 15:49:39 +0000

    linting

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,10 +1,10 @@
-import HelloWorld from "../containers/HelloWorld";
-import getInitialData from "./getInitialData";
-import services from "../lib/config/services";
+import HelloWorld from '../containers/HelloWorld';
+import getInitialData from './getInitialData';
+import services from '../lib/config/services';
 
 const serviceRegex = Object.keys(services)
-  .filter(serviceName => serviceName !== "default")
-  .join("|");
+  .filter(serviceName => serviceName !== 'default')
+  .join('|');
 
 // this gets passed to universal-react-app which passes the regex on to react-router-config
 export const regexPath = `/:service(${serviceRegex})/:id?`;
@@ -14,8 +14,8 @@ const routes = [
     path: regexPath, // the path for react-router to match against with named params EG: :service
     exact: true,
     component: HelloWorld, // the component you want to render with the data from getInitialData
-    getInitialData // a method which defines the URL to get data from and the params to pass through to the component
-  }
+    getInitialData, // a method which defines the URL to get data from and the params to pass through to the component
+  },
 ];
 
 export default routes;
