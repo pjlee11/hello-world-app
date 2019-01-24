566caadc95684f78a79fa786e641a211a837cb1c
Phil Lee <phil.lee.ext@bbc.co.uk>
2019-01-24 18:29:05 +0000

    ensure a the application sets up styled-components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import Document from './app/components/Document';
 import routes, { regexPath } from './app/routes';
+import { ServerStyleSheet } from 'styled-components';
 
 // setup global variables for simorgh-renderer to use at build time
 global.documentComponent = Document;
 global.regexPath = regexPath;
 global.routes = routes;
+global.sheet = new ServerStyleSheet();
 
 // inject simorgh-renderer/index and instantly execute
 require('simorgh-renderer/index');
