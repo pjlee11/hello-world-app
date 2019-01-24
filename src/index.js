97bbb03a26cad4703a7b59fd8ba0433857732256
Phil Lee <phil.lee.ext@bbc.co.uk>
2019-01-24 15:51:23 +0000

    linting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import Document from "./app/components/Document";
-import routes, { regexPath } from "./app/routes";
+import Document from './app/components/Document';
+import routes, { regexPath } from './app/routes';
 
 // setup global variables for simorgh-renderer to use at build time
 global.documentComponent = Document;
@@ -7,4 +7,4 @@ global.regexPath = regexPath;
 global.routes = routes;
 
 // inject simorgh-renderer/index and instantly execute
-require("simorgh-renderer/index");
+require('simorgh-renderer/index');
