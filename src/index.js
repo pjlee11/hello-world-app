d220008f5261363fac715dd8c7bf220ffb298c86
Phil Lee <phil.lee.ext@bbc.co.uk>
2019-01-25 10:51:06 +0000

    explain style-components singleton and rename datapath global

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import routes, { regexPath } from './app/routes';
 import { ServerStyleSheet } from 'styled-components';
 
 // setup global variables for simorgh-renderer to use at build time
-global.documentComponent = Document;
-global.regexPath = regexPath;
+global.documentComponent = Document; // a <html> document React component
+global.dataPathRegex = regexPath; // the regex path
 global.routes = routes;
-global.sheet = new ServerStyleSheet();
+
+/*
+ * 'styled-components' needs to be a singleton so it is set as a dependency of the top level application and the
+ * parts of it that 'simorgh-renderer' requires are injected as globals EG: ServerStyleSheet()
+ */
+global.ServerStyleSheet = ServerStyleSheet;
 
 // inject simorgh-renderer/index and instantly execute
 require('simorgh-renderer/index');
