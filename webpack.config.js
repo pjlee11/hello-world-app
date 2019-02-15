6f7a12b965be88dbde821e0b76a56e39d1134413
Phil Lee <phil.lee.ext@bbc.co.uk>
2019-02-15 11:04:34 +0000

    add comment regarding global.appDirectory

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 const dotenv = require('dotenv');
 
+// set the application root path to be set as `context` in the webpack config
 global.appDirectory = fs.realpathSync(process.cwd());
 
 // Load in environment variables configured in `.env` file. Be aware the `.env` committed is changed by bake-scripts when on real servers.
